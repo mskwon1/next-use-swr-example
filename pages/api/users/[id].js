d64ef8ce939b70e9e2318dc5cd163743b7d8f775
mskwon1 <mageeeeek@gmail.com>
2021-06-22 01:24:14 +0900

    edit api to use temp json file

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,8 +1,18 @@
-import { users } from '../../../store/users';
 import _ from 'lodash';
+import { promises as fsp } from 'fs';
 
-export default function handler(req, res) {
+const userFilePath = new URL('./users.json', import.meta.url);
+
+export default async function handler(req, res) {
+  const users = await loadUsers();
   const user = _.find(users, { id: _.toNumber(req.query.id) });
 
   return res.status(200).json(user);
+}
+
+async function loadUsers() {
+  let users = await fsp.readFile(userFilePath, 'utf-8');
+  users = JSON.parse(users);
+
+  return users;
 }
--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,5 +1,39 @@
-import { users } from '../../../store/users';
+import { promises as fsp } from 'fs';
+import _ from 'lodash';
+
+const userFilePath = new URL('./users.json', import.meta.url);
 
 export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { name, age } = req.body;
+    const newUser = await createUser(name, age);
+
+    return res.status(200).json(newUser);
+  }
+
+  const users = await loadUsers();
   return res.status(200).json(users);
+}
+
+async function createUser(name, age) {
+  const users = await loadUsers();
+  const lastUser = _.maxBy(users, 'id');
+
+  const newUser = {
+    id: _.toNumber(lastUser.id) + 1,
+    name: name,
+    age: age
+  }
+
+  const newUsers = _.concat(users, newUser);
+  await fsp.writeFile(userFilePath, JSON.stringify(newUsers));
+
+  return newUser;
+}
+
+async function loadUsers() {
+  let users = await fsp.readFile(userFilePath, 'utf-8');
+  users = JSON.parse(users);
+
+  return users;
 }
